9d44989d1e917adf3d431ed0acd2903fa81dd9da
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-09 19:41:51 -0500

    Update frontend.js

--- webserver/frontend/frontend.js
+++ webserver/frontend/frontend.js
@@ -4,7 +4,7 @@ import { ThemeProvider} from "theme-ui";
 import theme from "./theme";
 import Main from "./Main";
 
-exports.App = async () => {
+function App() {
   return (
     <ThemeProvider theme={theme}>
       <ColorMode />
