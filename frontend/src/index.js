b4c707ce5f3433962659cd054256f43a6f2c2aac
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-19 19:48:04 -0500

    move index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { ThemeProvider } from "theme-ui";
 import { useColorMode } from 'theme-ui'
-import theme from "./theme";
-import Main from "./Main";
+import theme from "../theme";
+import Main from "../Main";
 
 function App() {
   return (
