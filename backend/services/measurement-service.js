f41ac5c4048e4c7e218c4a65d51370654899fbf8
rafaelmuylaert <russo.simpson@gmail.com>
2024-02-21 21:30:52 -0500

    Update measurement-service.js

--- backend/services/measurement-service.js
+++ backend/services/measurement-service.js
@@ -5,22 +5,30 @@ const SecondlyMeasurement = require("../models/measurements/secondly-measurement
 
 exports.setMeasurement = async (measurementdata, queryFilter) => {
     const lastDailyMeasurement = await DailyMeasurement.findOne(queryFilter).sort({ timestamp: -1 });
-    const date = new Date();
+    const date = new Date().getTime();
+	
     var update = false;
     if (lastDailyMeasurement) {
-        if(lastDailyMeasurement.timestamp.getDate() < date.getDate()){update = true;}
+        if(date - lastDailyMeasurement.timestamp.getTime() > 86400000){update = true;}
     } 
     else {update = true;}
     if(update){DailyMeasurement.create(measurementdata);}
     
     const lastHourlyMeasurement = await HourlyMeasurement.findOne(queryFilter).sort({ timestamp: -1 });
-    var update = false;
+    update = false;
     if (lastHourlyMeasurement) {
-        if (lastHourlyMeasurement.timestamp.getHours() < date.getHours()){update = true;}
+        if (date - lastHourlyMeasurement.timestamp.getTime() < 3600000){update = true;}
     } 
     else {update = true;}
     if(update){HourlyMeasurement.create(measurementdata);}
-    MinutelyMeasurement.create(measurementdata);
+	
+	const lastMinutelyMeasurement = await MinutelyMeasurement.findOne(queryFilter).sort({ timestamp: -1 });
+    update = false;
+    if (lastMinutelyMeasurement) {
+        if (date - lastMinutelyMeasurement.timestamp.getTime() < 60000){update = true;}
+    } 
+    else {update = true;}
+    if(update){MinutelyMeasurement.create(measurementdata);}
     SecondlyMeasurement.create(measurementdata);
     result = [];
     return result;
